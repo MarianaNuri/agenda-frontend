1462a674edc4476798337908ec11187af2d33b6b
MarianaNuri <l24270017@tuxtla.tecnm.mx>
2026-05-18 23:34:53 -0600

    Change deleteContactService to use POST method
    Updated deleteContactService to use POST method instead of DELETE and included userId in the request.

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -97,14 +97,18 @@ export async function updateContactService(id, contactData, userId) {
 
 
 /**
- * Eliminar un contacto asignado al usuario.
+ * Eliminar un contacto asignado al usuario (Cumpliendo método POST de la rúbrica).
  * @param {number|string} id - ID del contacto a borrar
- * @param {number|string} userId - ID del usuario logueado 🔍 NUEVO PARÁMETRO
+ * @param {number|string} userId - ID del usuario logueado
  */
 export async function deleteContactService(id, userId) {
-  //CONCATENAMOS EL USUARIO_ID EN LA URL JUNTO AL ID DEL CONTACTO :P
-  const data = await apiRequest(`/contactos/eliminar.php?id=${id}&usuario_id=${userId}`, {
-    method: 'DELETE',
+  const formData = new FormData()
+  formData.append('id', id) // Enviamos el ID del contacto en el cuerpo de la petición
+
+  // Enviamos mediante POST y pasamos el usuario_id en la URL
+  const data = await apiRequest(`/contactos/eliminar.php?usuario_id=${userId}`, {
+    method: 'POST',
+    body: formData,
     auth: true,
   })
   return data
