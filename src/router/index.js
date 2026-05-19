0802681bc7559ed5ca506532013686bbbe880c4e
Eva del Carmen Gonzalez Santiz <L24270005@tuxtla.tecnm.mx>
2026-05-19 17:54:45 -0600

    importando webhash

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,11 @@
  * Las rutas de invitado (login, registro) usan meta: { guest: true }.
  */
 
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
